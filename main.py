1f53aeffe655e7e8a0e99400b12c24f4d8071eb4
azzainn <ahmedmohmedzain04@gmail.com>
2022-09-09 16:29:10 -0400

    cleaned up

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import requests
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+import os.path
 
 years = list(range(11,22))
 player_url = "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=20{}&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight="
 
-# bypasses nba's bot detection
-headers  = {
+headers  = {    # need this to bypass nba's bot detection
     'Connection': 'keep-alive',
     'Accept': 'application/json, text/plain, */*',
     'x-nba-stats-token': 'true',
@@ -19,25 +19,14 @@ headers  = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
 }
-
 for year in years:
-    year_url = player_url.format(str(year) + "-" + str(year + 1)) # need to do this weird formatting b/c url has year written as, for example, "2011-2012"
-    request_year_url = requests.get(url=year_url, headers=headers).json()
+    if not os.path.exists(f"player_stats_20{year}.csv"):    # saves time rather than writing to a csv every runtime
 
-    players = request_year_url["resultSets"][0]["rowSet"] # need this to navigate through html and extract only the player data
-    columns = request_year_url["resultSets"][0]["headers"]
+        year_url = player_url.format(str(year) + "-" + str(year + 1))   # need to do this weird formatting b/c url has year written as, for example, "2011-2012"
+        request_year_url = requests.get(url=year_url, headers=headers).json()
 
-    stats_df = pd.DataFrame(players, columns=columns)
-    stats_df.to_csv(f"player_stats_20{year}.csv", index=False)
+        players = request_year_url["resultSets"][0]["rowSet"]   # need this to navigate through html and extract only the player data
+        columns = request_year_url["resultSets"][0]["headers"]
 
-# # scraping player data 
-# for year in years:
-#     # changing url based on year
-#     url = player_url.format(str(year) + "-" + str(year + 1) )
-
-#     # requesting url and storing that as a json 
-#     r = requests.get(url=url, headers=headers).json()
-#     player_info = r["resultSets"]
-#     # writing text of url to an html file
-#     # with open(file=f"player/{year}.html", mode="w+", encoding="utf-8") as f:
-#     #     f.write(r.text)
+        stats_df = pd.DataFrame(players, columns=columns)
+        stats_df.to_csv(f"player_stats_20{year}.csv", mode="w+", index=False)
