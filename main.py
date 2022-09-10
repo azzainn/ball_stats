0ab56dc8b84c1edd39931655553df5c233264a7b
azzainn <ahmedmohmedzain04@gmail.com>
2022-09-09 22:02:45 -0400

    predict_stats now combines all stats from different years of a player into one df

--- main.py
+++ main.py
@@ -30,15 +30,25 @@ for year in years:
         columns = request_year_url["resultSets"][0]["headers"]
 
         stats_df = pd.DataFrame(players, columns=columns)
-        del stats_df["PLAYER_ID"]
+
+        del stats_df["PLAYER_ID"]   # cleans up table a little
         del stats_df["TEAM_ID"]
         del stats_df["WNBA_FANTASY_PTS"]
         del stats_df["WNBA_FANTASY_PTS_RANK"]
+
         stats_df.to_csv(f"player_stats_20{year}.csv", mode="w+", index=False)
 
-def name_search():
-    player_name = input("Whose predictions would you like to see? (First Last)").title()
-    players = pd.read_csv("player_stats_2011.csv")
-    print(players)
+# player_name = input("Whose predictions would you like to see? (First Last)").title()
 
-name_search()
+def predict_stats(player_name):
+    player_stats = []   # not initializing a df to save memory
+    for year in years:
+        stats_df_year = pd.read_csv(f"player_stats_20{year}.csv")
+        player_stats_year = stats_df_year.loc[stats_df_year["PLAYER_NAME"].str.contains(player_name, case=False)].iloc[0]
+        
+        if not player_stats_year.empty:
+            player_stats.append(player_stats_year)
+    player_stats_df = pd.DataFrame(player_stats)
+    # print(player_stats_df)
+
+predict_stats("Stephen Curry")
